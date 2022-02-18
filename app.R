b97cf1d0a614220bd0f9efcc6a30c271e6e141c3
Akash Magnadia <akash.magnadia@gmail.com>
2022-02-17 20:37:45 -0600

    Made ideal visual

--- app.R
+++ app.R
@@ -1,4 +1,130 @@
+#libraries to include
 
+library(shiny)
+library(shinyWidgets)
+library(shinyjs)
+library(shinydashboard)
+library(ggplot2)
+library(lubridate)
+library(scales)
+library(DT)
+library(tidyr)
+library(tidyverse)
+library(leaflet)
+
+
+# # assume all of the tsv files in this directory are data of the same kind that I want to visualize
+# entriesData <- do.call(rbind, lapply(list.files(pattern = "*Totals.tsv"), read.delim))
+# 
+# locData <- do.call(rbind, lapply(list.files(pattern = "*Stops.tsv"), read.delim))
+# locData <- data.frame(locData)
+# 
+# # convert the dates to the internal format
+# entriesData$fullDate <- entriesData$date
+# entriesData$newDate <- as.Date(entriesData$fullDate, "%m/%d/%Y")
+# entriesData$Date <- NULL
+# 
+# # add year day month column
+# entriesData <- separate(data = entriesData, col = date, into = c("month", "date", "year"), sep = "/")
+# 
+# # convert the columns to numeric
+# entriesData[ c("month", "date", "year")] <- sapply(entriesData[ c("month", "date", "year")],as.numeric)
+# 
+# add_monthChar <- function(df) {
+#   # add new column for month that contains char
+#   df$monthChar[df$month == 1] <- "Jan"
+#   df$monthChar[df$month == 2] <- "Feb"
+#   df$monthChar[df$month == 3] <- "Mar"
+#   df$monthChar[df$month == 4] <- "Apr"
+#   df$monthChar[df$month == 5] <- "May"
+#   df$monthChar[df$month == 6] <- "Jun"
+#   df$monthChar[df$month == 7] <- "Jul"
+#   df$monthChar[df$month == 8] <- "Aug"
+#   df$monthChar[df$month == 9] <- "Sep"
+#   df$monthChar[df$month == 10] <- "Oct"
+#   df$monthChar[df$month == 11] <- "Nov"
+#   df$monthChar[df$month == 12] <- "Dec"
+#   
+#   df
+# }
+# 
+# entriesData <- add_monthChar(entriesData)
+# 
+# # parse to days in the week
+# entriesData$dayChar <- weekdays(entriesData$newDate)
+# 
+# # add new column for week that contains int
+# add_dayCharToDay <- function(df) {
+#   df$day[df$dayChar == "Monday"] <- 1
+#   df$day[df$dayChar == "Tuesday"] <- 2
+#   df$day[df$dayChar == "Wednesday"] <- 3
+#   df$day[df$dayChar == "Thursday"] <- 4
+#   df$day[df$dayChar == "Friday"] <- 5
+#   df$day[df$dayChar == "Saturday"] <- 6
+#   df$day[df$dayChar == "Sunday"] <- 7
+#   
+#   df
+# }
+# entriesData <- add_dayCharToDay(entriesData)
+# 
+# # add new column for char entries that contains comma in the number
+# entriesData$ridesChar <- formatC(entriesData$rides, format = "d", big.mark = ",")
+# 
+# # turn to data frame
+# all_data_df <- data.frame(entriesData)
+# 
+# all_data_df$stationname[all_data_df$stationname == "OHare Airport"] <- "O'Hare"
+# 
+# # transfer data from location file to main data frame
+# all_data_df$long <- locData$long[match(all_data_df$stationname, locData$STATION_NAME)]
+# all_data_df$lat <- locData$lat[match(all_data_df$stationname, locData$STATION_NAME)]
+# 
+# # getting rid of the stations with no location info
+# all_data_df <- subset(all_data_df, !is.na(long))
+# 
+# all_data_df$RedLine <- locData$RED[match(all_data_df$stationname, locData$STATION_NAME)]
+# all_data_df$BlueLine <- locData$BLUE[match(all_data_df$stationname, locData$STATION_NAME)]
+# all_data_df$GreenLine <- locData$G[match(all_data_df$stationname, locData$STATION_NAME)]
+# all_data_df$BrownLine <- locData$BRN[match(all_data_df$stationname, locData$STATION_NAME)]
+# all_data_df$PurpleLine <- locData$Pexp[match(all_data_df$stationname, locData$STATION_NAME)]
+# all_data_df$YellowLine <- locData$Y[match(all_data_df$stationname, locData$STATION_NAME)]
+# all_data_df$PinkLine <- locData$Pnk[match(all_data_df$stationname, locData$STATION_NAME)]
+# all_data_df$OrangeLine <- locData$O[match(all_data_df$stationname, locData$STATION_NAME)]
+# 
+# Red_df <- subset(all_data_df, all_data_df$RedLine == "true")
+# Blue_df <- subset(all_data_df, all_data_df$BlueLine == "true")
+# Green_df <- subset(all_data_df, all_data_df$GreenLine == "true")
+# Brown_df <- subset(all_data_df, all_data_df$BrownLine == "true")
+# Purple_df <- subset(all_data_df, all_data_df$PurpleLine == "true")
+# Yellow_df <- subset(all_data_df, all_data_df$YellowLine == "true")
+# Pink_df <- subset(all_data_df, all_data_df$PinkLine == "true")
+# Orange_df <- subset(all_data_df, all_data_df$OrangeLine == "true")
+# 
+# station_names <- unique(all_data_df[c("stationname")])
+# station_names <- station_names[order(station_names$stationname), ]
+# 
+# # create dataframe for a particular station
+# create_station_df <- function(station) {
+#   subset(all_data_df, all_data_df$stationname == station)
+# }
+# 
+# # get data frame for a particular station
+# get_station_df <- function(station) {
+#   station_df[[1]][which(station_names == station)]
+# }
+# 
+# # create a dataframe for each station
+# station_df <- list(lapply(station_names, create_station_df))
+# 
+# starting_df <- subset(all_data_df, all_data_df$newDate == "2021-08-23")
+# 
+# every_nth = function(n) {
+#   return(function(x) {x[c(TRUE, rep(FALSE, n - 1))]})
+# }
+
+# position to view all CTA stations on map
+default_long <- -87.658753
+default_lat <- 41.866568
 
 # Create the shiny application
 ui <- dashboardPage(
@@ -31,45 +157,59 @@ ui <- dashboardPage(
                 position = "left",
                 sidebarPanel(
                   style = "margin-top:100%; padding: 15px;",
-                  div(checkboxInput("single_date_check",
-                                    label = "Single Date",
-                                    value = TRUE
-                                    )
-                      ),
-                  div(dateInput("single_date_input",
+                  checkboxInput("single_date_check",
                                 label = "Single Date",
-                                value = "2021-08-23",
-                                min = "2001-01-01",
-                                max = "2021-11-30",
-                                format = "m / dd / yyyy"
-                                )
-                      ),
-                  div(checkboxInput("range_date_check",
-                                    label = "Range of Date(s)",
-                                    value = FALSE
-                                    )
-                      ),
-                  div(dateInput("range_start_date_input",
-                                label = "Start Date",
-                                value = "2020-08-23",
-                                min = "2001-01-01",
-                                max = "2021-11-30",
-                                format = "m / dd / yyyy"
-                  )
+                                value = TRUE
                   ),
-                  div(dateInput("range_end_date_input",
-                                label = "End Date",
-                                value = "2021-08-23",
-                                min = "2001-01-01",
-                                max = "2021-11-30",
-                                format = "m / dd / yyyy"
-                  )
+                  dateInput("single_date_input",
+                            label = "Single Date",
+                            value = "2021-08-23",
+                            min = "2001-01-01",
+                            max = "2021-11-30",
+                            format = "m / dd / yyyy"
                   ),
-                  width = 2
+                  checkboxInput("range_date_check",
+                                label = "Range of Date(s)",
+                                value = FALSE
+                  ),
+                  dateInput("range_start_date_input",
+                            label = "Start Date",
+                            value = "2020-08-23",
+                            min = "2001-01-01",
+                            max = "2021-11-30",
+                            format = "m / dd / yyyy"
+                  ),
+                  dateInput("range_end_date_input",
+                            label = "End Date",
+                            value = "2021-08-23",
+                            min = "2001-01-01",
+                            max = "2021-11-30",
+                            format = "m / dd / yyyy"
+                  ),
+                  selectInput("stations",
+                              label = "Stations",
+                              choices = station_names,
+                              selected = "UIC-Halsted"
+                  ),
+                  actionButton("prev_day_btn",
+                               label = "Previous Day"
+                  ),
+                  actionButton("next_day_btn",
+                               label = "Next Day"
+                  ),
+                  width = 1
                 ),
                 mainPanel(
-                  h2("test"),
-                  width = 10
+                  tags$style(type = "text/css", "#mainMap {height: calc(100vh - 80px) !important;}"),
+                  tags$style(type = "text/css", "#mainBarGraph {height: calc(70vh - 80px) !important;}"),
+                  fluidRow(
+                    column(6,
+                           leafletOutput("mainMap")),
+                    column(6,
+                           div(plotOutput("mainBarGraph")),
+                           uiOutput("mainTable"))
+                  ),
+                  width = 11
                   )
                 )
               )
@@ -110,6 +250,77 @@ server <- function(input, output, session) {
       gradientCol <- "#984ea3"
     }
   }  
+  
+  #################################################################
+  
+  getBaseMap <- function() {
+    # creating initial map
+    m <- leaflet(options = leafletOptions(preferCanvas = T)) %>%
+      addTiles() %>%  # Add default OpenStreetMap map tiles
+      setView(default_long, default_lat, zoom = 12)
+    m
+  }
+  
+  addCirclesMarkers <- function() {
+    m <- getBaseMap()
+    
+    m <- m %>% addCircleMarkers(data = locData, ~long, ~lat, 
+                                popup = locData$STOP_NAME, 
+                                weight = 3, 
+                                radius = 10,
+                                color = "#70FFF8", 
+                                stroke = F, 
+                                fillOpacity = 0.50)
+    
+    m
+  }
+  
+  #################################################################
+  
+  # create a bar graph
+  
+  output$mainBarGraph <- renderPlot({
+    ggplot(data = starting_df, aes(x = stationname, y = rides)) + 
+      geom_bar(stat = 'identity', aes(fill = rides)) +
+      scale_y_continuous(breaks = scales::pretty_breaks(n = 10), labels = comma) +
+      labs(x = "Stations",
+           y = "Entries") +
+      ggtitle(paste("Entries Station")) +
+      theme(legend.position = "none") + 
+      coord_flip()
+  })
+  
+  #################################################################
+  
+  # create a data table
+  
+  output$mainTable <- renderUI({
+    # format the table layout
+    
+    div(
+      tags$head(
+        tags$style(
+          HTML('
+          .datatables {
+            width: inherit !important;
+          }
+           ')
+        )
+      ),
+      
+      datatable(
+        locData,
+        options = list(
+          pageLength = 10,
+          lengthChange = FALSE,
+          scrollX = TRUE,
+          dom = 'tp',
+          columnDefs = list(list(className = 'dt-center', targets = "_all"))
+        ),
+        rownames = FALSE
+      )
+    )
+  })
   
   #################################################################
   
@@ -201,6 +412,12 @@ server <- function(input, output, session) {
   
   #################################################################
   
+  output$mainMap <- renderLeaflet({
+    addCirclesMarkers()
+  })
+  
+  #################################################################
+  
   observeEvent(input$single_date_check, {
     if (input$single_date_check) {
       updateCheckboxInput(session, "range_date_check", value = FALSE)
@@ -216,6 +433,48 @@ server <- function(input, output, session) {
       shinyjs::disable("single_date_input")
       shinyjs::enable("range_start_date_input")
       shinyjs::enable("range_end_date_input")
+    }
+  })
+  
+  observeEvent(input$prev_day_btn, {
+    if (input$single_date_check) {
+      updateDateInput(session,
+                      "single_date_input",
+                      value = as.Date(input$single_date_input) - 1
+      )
+    }
+    
+    if (input$range_date_check) {
+      updateDateInput(session,
+                      "range_start_date_input",
+                      value = as.Date(input$range_start_date_input) - 1
+      )
+      
+      updateDateInput(session,
+                      "range_end_date_input",
+                      value = as.Date(input$range_end_date_input) - 1
+      )
+    }
+  })
+  
+  observeEvent(input$next_day_btn, {
+    if (input$single_date_check) {
+      updateDateInput(session,
+                      "single_date_input",
+                      value = as.Date(input$single_date_input) + 1
+      )
+    }
+    
+    if (input$range_date_check) {
+      updateDateInput(session,
+                      "range_start_date_input",
+                      value = as.Date(input$range_start_date_input) + 1
+      )
+      
+      updateDateInput(session,
+                      "range_end_date_input",
+                      value = as.Date(input$range_end_date_input) + 1
+      )
     }
   })
 }
